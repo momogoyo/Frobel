7d30f8b9d7bdaeb783f48e4548db6e2c381e5445
momogoyo <metheduxk@gmail.com>
2024-05-27 23:51:08 +0900

    Initial Commit

---
+++ Frobel/FrobelApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrobelApp.swift
+//  Frobel
+//
+//  Created by 현유진 on 5/27/24.
+//
+
+import SwiftUI
+
+@main
+struct FrobelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
