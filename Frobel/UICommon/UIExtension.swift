cfac24b0fdc5d2173850ec5df34039975d122b6c
momogoyo <metheduxk@gmail.com>
2024-05-28 00:11:40 +0900

    🎉 프로젝트 세팅

---
+++ Frobel/UICommon/UIExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtension.swift
+//  Frobel
+//
+//  Created by 현유진 on 5/28/24.
+//
+
+import SwiftUI
+
+struct UIExtension: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtension()
+}
