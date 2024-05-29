a048780aebc4be82f9db937a97411cfb6a50e5a5
momogoyo <metheduxk@gmail.com>
2024-05-30 08:43:51 +0900

    ✨CircularStd 열거 타입 선언, Font, CGFloat 확장

--- Frobel/UICommon/UIExtension.swift
+++ Frobel/UICommon/UIExtension.swift
@@ -7,12 +7,62 @@
 
 import SwiftUI
 
-struct UIExtension: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+enum CircularStd: String {
+    case regular = "CircularStd-Book"
+    case medium = "CircularStd-Medium"
+    case bold = "CircularStd-Bold"
+    case black = "CircularStd-Black"
+}
+
+extension Font {
+    static func customfont(_ font: CircularStd, fontSize: CGFloat) -> Font {
+        custom(font.rawValue, size: fontSize)
     }
 }
 
-#Preview {
-    UIExtension()
+extension CGFloat {
+    static var screenWidth: Double {
+        return UIScreen.main.bounds.size.width
+    }
+    
+    static var screenHeight: Double {
+        return UIScreen.main.bounds.size.height
+    }
+    
+    static func widthPer(per: Double) -> Double {
+        return screenWidth * per
+        // 375 * 0.5
+    }
+    
+    static func heightPer(per: Double) -> Double {
+        return screenHeight * per
+    }
+    
+    static var topInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.top
+        }
+        return 0.0
+    }
+    
+    static var bottomInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.bottom
+        }
+        return 0.0
+    }
+    
+    static var horizontalInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.left + keyWindow.safeAreaInsets.right
+        }
+        return 0.0
+    }
+    
+    static var verticalInsets: Double {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            return keyWindow.safeAreaInsets.top + keyWindow.safeAreaInsets.bottom
+        }
+        return 0.0
+    }
 }
